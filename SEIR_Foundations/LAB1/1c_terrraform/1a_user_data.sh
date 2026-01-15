e945942bde227ee6608e4a4a7dd748109c0d49a2
Kevin Willocks <kevinwillocks@gmail.com>
2026-01-15 00:06:55 -0600

    Added argument to force user data script to re-run Updated 1a_user_data.sh

--- SEIR_Foundations/LAB1/1c_terrraform/1a_user_data.sh
+++ SEIR_Foundations/LAB1/1c_terrraform/1a_user_data.sh
@@ -12,7 +12,7 @@ import pymysql
 from flask import Flask, request
 
 REGION = os.environ.get("AWS_REGION", "us-east-1")
-SECRET_ID = os.environ.get("SECRET_ID", "lab2/rds/mysql")
+SECRET_ID = os.environ.get("SECRET_ID", "lab1a/rds/mysql")
 
 secrets = boto3.client("secretsmanager", region_name=REGION)
 
@@ -44,52 +44,64 @@ def home():
 
 @app.route("/init")
 def init_db():
-    c = get_db_creds()
-    host = c["host"]
-    user = c["username"]
-    password = c["password"]
-    port = int(c.get("port", 3306))
+    try:
+        c = get_db_creds()
+        host = c["host"]
+        user = c["username"]
+        password = c["password"]
+        port = int(c.get("port", 3306))
 
-    # connect without specifying a DB first
-    conn = pymysql.connect(host=host, user=user, password=password, port=port, autocommit=True)
-    cur = conn.cursor()
-    cur.execute("CREATE DATABASE IF NOT EXISTS labdb;")
-    cur.execute("USE labdb;")
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS notes (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            note VARCHAR(255) NOT NULL
-        );
-    """)
-    cur.close()
-    conn.close()
-    return "Initialized labdb + notes table."
+        # connect without specifying a DB first
+        conn = pymysql.connect(host=host, user=user, password=password, port=port, autocommit=True)
+        cur = conn.cursor()
+        cur.execute("CREATE DATABASE IF NOT EXISTS labdb;")
+        cur.execute("USE labdb;")
+        cur.execute("""
+            CREATE TABLE IF NOT EXISTS notes (
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                note VARCHAR(255) NOT NULL
+            );
+        """)
+        cur.close()
+        conn.close()
+        return "Initialized labdb + notes table."
+    except Exception as e:
+        traceback.print_exc()
+        return f"init failed: {str(e)}" , 500
 
 @app.route("/add", methods=["POST", "GET"])
 def add_note():
-    note = request.args.get("note", "").strip()
-    if not note:
-        return "Missing note param. Try: /add?note=hello", 400
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("INSERT INTO notes(note) VALUES(%s);", (note,))
-    cur.close()
-    conn.close()
-    return f"Inserted note: {note}"
+    try:
+        note = request.args.get("note", "").strip()
+        if not note:
+            return "Missing note param. Try: /add?note=hello", 400
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute("INSERT INTO notes(note) VALUES(%s);", (note,))
+        cur.close()
+        conn.close()
+        return f"Inserted note: {note}"
+    except Exception as e:
+        traceback.print_exc()
+        return f"init failed: {str(e)}" , 500
 
 @app.route("/list")
 def list_notes():
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT id, note FROM notes ORDER BY id DESC;")
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-    out = "<h3>Notes</h3><ul>"
-    for r in rows:
-        out += f"<li>{r[0]}: {r[1]}</li>"
-    out += "</ul>"
-    return out
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute("SELECT id, note FROM notes ORDER BY id DESC;")
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+        out = "<h3>Notes</h3><ul>"
+        for r in rows:
+            out += f"<li>{r[0]}: {r[1]}</li>"
+        out += "</ul>"
+        return out
+    except Exception as e:
+        traceback.print_exc()
+        return f"init failed: {str(e)}" , 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
@@ -102,7 +114,7 @@ After=network.target
 
 [Service]
 WorkingDirectory=/opt/rdsapp
-Environment=SECRET_ID=lab2/rds/mysql
+Environment=SECRET_ID=lab1a/rds/mysql
 ExecStart=/usr/bin/python3 /opt/rdsapp/app.py
 Restart=always
 
